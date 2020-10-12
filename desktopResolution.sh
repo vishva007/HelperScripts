d0256a051125bb03c5bb40e8bf2b32795cdd5c23
avishvat <6756036+vishva007@users.noreply.github.com>
2020-10-12 23:31:16 +0530

    Create desktopResolution.sh

---
+++ desktopResolution.sh
@@ -0,0 +1,5 @@
+#alias for desktop resolution of second desktop
+#xrandr and replace "DP-2" with respective 
+alias sc1="cvt 1360 768"
+alias sc2="xrandr --newmode \"1360x768_60.00\"   84.75  1360 1432 1568 1776  768 771 781 798 -hsync +vsync"
+alias sc3="xrandr --addmode DP-2 \"1360x768_60.00\""
